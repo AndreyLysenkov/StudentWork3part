882e9424288529a09caa49a28ea36659c29e57fd
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-12-24 02:45:04 +0300

    doing lab6 assembler

--- assembler/lab6/lab6.asm
+++ assembler/lab6/lab6.asm
@@ -7,7 +7,9 @@
 	Aout db 'Enter a = $'
 	Bout db 'Enter b = ', '$'
 	Xout db 'Answer x =', '$'
-
+	minus db '-$'
+	errorNmb db 'Number incorrect'
+	buffer  db 6,7 dup (?)
 .code
 
 mPush macro
@@ -44,33 +46,75 @@ mPrntStr macro string
 	mPop
 endm
 
+mReadNumb macro value, buffer
+	;; Чтение числа
+local positive, positive2, ext, wrong, getDgt
+	mov ah, 0Ah
+	xor di, di
+	mov dx, offset buffer
+	int 21h
+	mov dl, 0Ah
+	mov ah, 02h
+	int 21h
+	mov si, offset buff+2
+	cmp byte ptr [si], "-"
+	jnz positive
+	mov di, 1
+	inc si
+positive:
+	xor ax, ax
+	mov bx, 0Ah
+getDgt:
+	mov cl, byte ptr[si]
+	cmp cl, 0Dh
+	jz ext
+	cmp cl, '0'
+	jl wrong
+	cmp cl, '9'
+	jg wrong
+	sub cl, '0'
+	mul bx
+	add ax, cx
+	inc si
+	jmp getDgt	
+wrong:
+	mPrntStr errorNmb
+ext:
+	cmp di, 1
+	jnz positive2
+	neg ax
+positive2:
+	mov value, ax
+endm
 
-mPrntNumb macro 
-local positive, findDigit, getDgt, prntDgt
+mPrntNumb macro value
+	;; Печать числа
+local positive, findDigit, gtDgt, prntDgt
+	mov ax, value
 	test ax, ax
 	jns positive
-	mPrntStr '-', '$'
+	mPrntStr minus
 	neg ax
 positive:
 	xor cx, cx 
 	mov bx, 0Ah
 findDigit:
-	xor dx,dx 
+	xor dx, dx 
 	div bx
 	push dx
 	inc cx
-	test ax,ax
+	test ax, ax
 	jnz findDigit
-	mov ah,02h 
-getDgt:
+	mov ah, 02h 
+gtDgt:
 	pop dx
 	cmp dl, 9d
-	jbe mac4
+	jbe prntDgt
 	add dl,7d
 prntDgt:
-	add dl,'0'
+	add dl, '0'
 	int 21h 
-	loop mac3 
+	loop gtDgt
 endm 
 
 mSetPoint macro row: REQ, column: REQ
@@ -85,17 +129,43 @@ mSetPoint macro row: REQ, column: REQ
 endm
 
 start:
-mov ax,@data
-mov ds,ax
-mClrScr
-mSetPoint 10, 10
-mPrntStr Aout
-mov ax, -5d
-mPrntNumb
-jmp exit
+	mov ax,@data
+	mov ds,ax
+	xor ax, ax
+	xor dx, dx
+	mClrScr
+	mSetPoint 5, 3
+	mPrntStr Aout
+	mReadNumb a, buffer
+	mSetPoint 6, 3
+	mReadNumb b, buffer
+	mov ax,a
+	cmp ax,b
+	je equal
+	jl less
+	jg more
+equal:
+	xor ax, ax
+	jmp result
+less:
+	xor ax, ax
+	jmp result
+more:
+	xor ax, ax
+	jmp result
 
-
+result:
+	;; Вывод результата
+	xor ax, ax
+	mSetPoint 7, 3
+	mPrntStr Xout
+	mPrntNumb x
+	xor ax, ax
+	mov ah, 7h
+	int 21h
+	jmp exit
 exit:
-mov ax,4C00h
-int 21h
+	;; выход из программы
+	mov ax,4C00h
+	int 21h
 end start
