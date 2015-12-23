562abbb5459bc2daea7672426d7a7091b9d69343
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-12-23 19:47:37 +0300

    assembler lab3 done

--- assembler/lab3/lab3.asm
+++ assembler/lab3/lab3.asm
@@ -1,81 +1,81 @@
 .model small
 .stack 100h
 .data
-message db 'Hard is the full step', '$'
-fullname db 'Varro, Mark Terence', '$'
-lifeyears db '116-27 years. BC', '$'
-studentsurname db 'Turchenko', '$'
-groupnumber db 'ITD-31', '$'
-faculty db 'FNC', '$'
+	message1 db 'Hard is the full step', '$'
+	message2 db 'Varro, Mark Terence', '$'
+	message3 db '116-27 years. BC', '$'
+	myname db 'Lysenkov', '$'
+	mygroup db 'IT-31', '$'
+	myfaculty db 'FNC', '$'
 .code
 start:
     mov ax, @data
     mov ds, ax
     mov ax, 0600h
-    mov bh, 0Ch    ;
+    mov bh, 4eh
     mov cx, 0000
     mov dx, 184fh
     int 10h       ;Конец очистки экрана
-    mov ah, 2
-    mov dh, 10   ;номер строки - 10
-    mov dl, 26   ;номер столбца - 26
-    mov bh, 0
-    int 10h      ;координаты message
-    mov ah, 09h
-    mov dx, offset message
-    int 21h
-    mov ah, 2
-    mov dh, 11   ;номер строки - 11
-    mov dl, 26   ;номер столбца - 26
-    mov bh, 0
-    int 10h      ;координаты fullname
-    mov ah, 09h
-    mov dx, offset fullname
-    int 21h
-    mov ah, 2
-    mov dh, 12   ;номер строки - 12
-    mov dl, 26   ;номер столбца - 26
+    mov ah, 02h
+    mov dh, 10   ;номер строки
+    mov dl, 25  ;номер столбца
     mov bh, 0
     int 10h
     mov ah, 09h
-    mov dx, offset lifeyears
-    int 21h
-    mov ah, 2
-    mov dh, 0   ;номер строки - 0
-    mov dl, 0   ;номер столбца - 0
+    mov dx, offset message1
+    int 21h     ;вывод сообщения
+    mov ah, 02h
+    mov dh, 11
+    mov dl, 25
     mov bh, 0
-    int 10h      ;координаты lifeyears
+    int 10h
     mov ah, 09h
-    mov dx, offset studentsurname
+    mov dx, offset message2
     int 21h
-    mov ah, 2
-    mov dh, 0   ;номер строки - 0
-    mov dl, 74   ;номер столбца - 74
+    mov ah, 02h
+    mov dh, 12
+    mov dl, 25
     mov bh, 0
-    int 10h      ;координаты groupnumber
+    int 10h
     mov ah, 09h
-    mov dx, offset groupnumber
+    mov dx, offset message3
     int 21h
-    mov ah, 2
-    mov dh, 24   
-    mov dl, 0  
+    mov ah, 02h
+    mov dh, 0
+    mov dl, 0
     mov bh, 0
-    int 10h      ;координаты faculty
+    int 10h
     mov ah, 09h
-    mov dx, offset faculty
+    mov dx, offset myname
+    int 21h
+    mov ah, 02h
+    mov dh, 0
+    mov dl, 74
+    mov bh, 0
+    int 10h
+    mov ah, 09h
+    mov dx, offset mygroup
+    int 21h
+    mov ah, 02h
+    mov dh, 24
+    mov dl, 0
+    mov bh, 0
+    int 10h
+    mov ah, 09h
+    mov dx, offset myfaculty
 	int 21h
-	mov ah,02h
-	mov dh,24
-	mov dl,75
-	mov bh,00h
+	mov ah, 02h
+	mov dh, 24
+	mov dl, 73
+	mov bh, 0
 	int 10h
-	mov al,'!'
-	mov bl,10001100b
-	mov cx,5
-	mov ah,09h
+	mov ah, 09h
+	mov al, '!'
+	mov bl, 10001100b
+	mov cx, 5
 	int 10h
-	mov ah,7h
+	mov ah, 7h 
 	int 21h
-	mov ax,4c00h
+	mov ax, 4c00h 
 	int 21h
-	end start
+end start
