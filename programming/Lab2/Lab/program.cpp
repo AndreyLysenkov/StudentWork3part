a21441ff177b852b8c34e0d9c5cd04fa42323855
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-12-24 23:24:42 +0300

    i think i'm done with lab2

--- programming/Lab2/Lab/program.cpp
+++ programming/Lab2/Lab/program.cpp
@@ -1,4 +1,5 @@
 #include "Trial.h"
+#include "stack.h"
 #include <string>
 #include <iostream>
 using namespace std;
@@ -6,56 +7,18 @@ using namespace std;
 
 void main()
 {
-	cout << endl << "Trial -----" << endl;
-	Trial Trial1,
-		Trial2("3rd semester", "Math"),
-		Trial3(Trial2);
-	Trial1.Print();
-	Trial2.Print();
-	Trial3.Print();
-	Trial1.SetName("1st semester");
-	Trial1.SetSubject("Literature");
-	Trial1.Print();
-	Trial3.Print();
-
-	cout << endl  << "Exam -----" << endl;
-	Exam Exam1,
-		Exam2("2nd semester 1st module", "Assembler"),
-		Exam3(Exam2);
-	Exam1.Print();
-	Exam2.Print();
-	Exam3.Print();
-	Exam1.SetName("1st semester");
-	Exam1.SetSubject("Programming");
-	Exam1.SetMark(3);
-	Exam1.Print();
-	Exam3.Print();
-
-	cout << endl << "FinalExam -----" << endl;
-	FinalExam FinalExam1,
-		FinalExam2("2nd semester 1st module", "Assembler"),
-		FinalExam3(FinalExam2);
-	FinalExam1.Print();
-	FinalExam2.Print();
-	FinalExam3.Print();
-	FinalExam1.SetName("1st semester");
-	FinalExam1.SetSubject("Programming");
-	FinalExam1.SetMark(3);
-	FinalExam1.Print();
-	FinalExam3.Print();
-
-	cout << endl << "Test -----" << endl;
-	Test Test1,
-		Test2("3rd semester", "Math", 55, 100, 60),
-		Test3(Test1);
-	Test1.Print();
-	Test2.Print();
-	Test3.Print();
-	Test1.SetName("1st semester");
-	Test1.SetSubject("Literature");
-	Test1.Print();
-	Test3.Print();
-
+	Stack stash;
+	stash.Add(new Trial());
+	stash.Add(new Test());
+	stash.Add(new Exam());
+	stash.Add(new FinalExam());
+	stash.Show();
+	cout << "deleting 1st one -----" << endl;
+	stash.Get();
+	stash.Show();
+	cout << "adding one of them" << endl;
+	stash.Add(new Trial("Web-programming", "1st half"));
+	stash.Show();
 	int a;
 	cin >> a;
 }
