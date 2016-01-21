1528e7a79f80ed1d01d3b04385909ae7e47998ad
Andrey Lysenkov <lysenkov-ae@ya.ru>
2016-01-21 21:07:48 +0300

    task 3 done

--- programming/Lab8/lab/lab/Program.cs
+++ programming/Lab8/lab/lab/Program.cs
@@ -20,9 +20,12 @@ namespace lab
         static void Main(string[] args)
         {
             WriteLine("Program Lab Walpy");
+            WriteLine("Task1");
             Task1.Run();
+            WriteLine("Task2");
             Task2.Run();
-
+            WriteLine("Task3");
+            Task3.Run();
             Console.ReadLine();
         }
 
@@ -78,7 +81,68 @@ namespace lab
                 return (Math.Pow(numb / 10, 2) - (numb % 10)) == numb;
             }
 
+        }
 
+        static public class Task3
+        {
+
+            static int[] array1;
+
+            static int[] array2;
+
+            static public void Run()
+            {
+                WriteLine("Enter array1:");
+                EnterArray(ref array1);
+                WriteLine("Enter array2:");
+                EnterArray(ref array2);
+                Int64 numb1 = MakeNumber(array1);
+                Int64 numb2 = MakeNumber(array2);
+                WriteLine(String.Format("Summ of two numbers is {0}", numb1 + numb2));
+            }
+
+            static bool IsDigit(int numb)
+            {
+                return (numb <= 9) && (numb >= 0);
+            }
+
+            static int EnterNumb(string message)
+            {
+                int result = 0;
+                do
+                {
+                    WriteLine(message);
+                    result = Console.Read();
+                } while (IsDigit(result));
+                return result;
+            }
+
+            static void EnterArray(ref int[] array)
+            {
+                int size = EnterNumb("Enter size of array: ");
+                array = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = EnterNumb(String.Format("Enter {0} element: ", i));
+                }
+            }
+
+            static Int64 MakeNumber(int[] array, int basis = 10)
+            {
+                Int64 result = 0;
+                Int64 basicPow = 1;
+                for (int i = 0; i < array.Length; i++, basicPow *= basis)
+                {
+                    result += array[i] * basicPow;
+                }
+                return result;
+            }
+
+            //Напишите программу, которая вводит 
+            //c клавиатуры два непустых массива целых
+            //чисел в диапазоне от 0 до 9 и, 
+            //считая эти массивы десятичным представлением
+            //двух чисел, печатает их сумму.
         }
     }
 }
