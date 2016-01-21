35a37d3f7b6fa337355a63586cdf2e3b5d9043a8
Andrey Lysenkov <lysenkov-ae@ya.ru>
2016-01-21 20:43:25 +0300

    task 2 lab8 done

--- programming/Lab8/lab/lab/Program.cs
+++ programming/Lab8/lab/lab/Program.cs
@@ -20,38 +20,65 @@ namespace lab
         static void Main(string[] args)
         {
             WriteLine("Program Lab Walpy");
-            Task1();
+            Task1.Run();
+            Task2.Run();
 
             Console.ReadLine();
         }
 
-        static void Task1()
+        static public class Task1
         {
-            PrintFunction(BoolFunction);
-        }
 
-        static bool BoolFunction(bool a, bool b, bool c)
-        {
-            return (((a == b) == c) || !a);
-        }
-
-        static void SetOfTwo(int number, ref bool a, ref bool b, ref bool c)
-        {
-            a = (number / 4) == 1;
-            b = (number / 2 - (number / 4) * 2) == 1;
-            c = (number % 2) == 1;
-        }
-
-        static void PrintFunction(BooleanFunction func)
-        {
-            bool a = false, b = false, c = false;
-            WriteLine("   A      B      C      Result");
-            for (int i = 0; i < 8; i++)
+            static public void Run()
             {
-                SetOfTwo(i, ref a, ref b, ref c);
-                WriteLine(String.Format("{0,6} {1,6} {2,6} - {3,6}", a, b, c, func(a, b, c)));
+                PrintFunction(BoolFunction);
             }
+
+            static bool BoolFunction(bool a, bool b, bool c)
+            {
+                return (((a == b) == c) || !a);
+            }
+
+            static void SetOfTwo(int number, ref bool a, ref bool b, ref bool c)
+            {
+                a = (number / 4) == 1;
+                b = (number / 2 - (number / 4) * 2) == 1;
+                c = (number % 2) == 1;
+            }
+
+            static void PrintFunction(BooleanFunction func)
+            {
+                bool a = false, b = false, c = false;
+                WriteLine("   A      B      C      Result");
+                for (int i = 0; i < 8; i++)
+                {
+                    SetOfTwo(i, ref a, ref b, ref c);
+                    WriteLine(String.Format("{0,6} {1,6} {2,6} - {3,6}", a, b, c, func(a, b, c)));
+                }
+            }
+
         }
 
+        static public class Task2
+        {
+
+            public static void Run()
+            {
+                for (int i = 100; i < 1000; i++)
+                {
+                    if (IsSatisfy(i))
+                    {
+                        WriteLine(String.Format(" {0} ", i));
+                    }
+                }
+            }
+
+            static bool IsSatisfy(int numb)
+            {
+                return (Math.Pow(numb / 10, 2) - (numb % 10)) == numb;
+            }
+
+
+        }
     }
 }
