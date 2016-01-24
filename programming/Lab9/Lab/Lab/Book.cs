1ff41a107ec167f3f898bdb309f1d91f43c3750a
Andrey Lysenkov <lysenkov-ae@ya.ru>
2016-01-25 02:05:32 +0300

    still doing stuff

---
+++ programming/Lab9/Lab/Lab/Book.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    public class LibraryBook
+    {
+        public const string No_Author = "Undefinded";
+
+        public class Isbn
+        {
+            private static char separator = '-';
+
+            public int EanUcc
+            {
+                get;
+                private set;
+            }
+
+            public int RegistrationGroup
+            {
+                get;
+                private set;
+            }
+
+            public int RegistratorIndex
+            {
+                get;
+                private set;
+            }
+
+            public int PublisherIndex
+            {
+                get;
+                private set;
+            }
+
+            public int ControlIndex
+            {
+                get;
+                private set;
+            }
+
+            public Isbn(int ean_ucc = 978, int registration_group = 3, int registration_index = 16, int publisher_index = 148410, int control_index = 0)
+            {
+                this.EanUcc = ean_ucc;
+                this.RegistrationGroup = registration_group;
+                this.RegistratorIndex = registration_index;
+                this.PublisherIndex = publisher_index;
+                this.ControlIndex = control_index;
+            }
+
+            public override string ToString()
+            {
+                return
+                    this.EanUcc.ToString() + separator +
+                    this.RegistrationGroup + separator +
+                    this.RegistratorIndex + separator +
+                    this.PublisherIndex + separator +
+                    this.ControlIndex;
+            }
+        }
+
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public string Author
+        {
+            get;
+            private set;
+        }
+
+        public Isbn ISBN
+        {
+            get;
+            private set;
+        }
+
+        public int Quantity
+        {
+            get;
+            protected set;
+        }
+
+        private int Borrowed
+        {
+            get;
+            set;
+        }
+
+        private bool IsAtHand
+        {
+            get
+            {
+                return (Borrowed != 0);
+            }
+        }
+
+        public DateTime Published
+        {
+            get;
+            private set;
+        }
+
+        public virtual bool IsAvailible
+        {
+            get
+            {
+                return (this.Quantity != 0);
+            }
+        }
+
+        public string Content
+        {
+            get;
+            private set;
+        }
+
+        public LibraryBook(Isbn isbn, DateTime published, string title, string author = No_Author, int quantity = 0, string content = "not digitalized")
+        {
+            this.ISBN = isbn;
+            this.Title = title;
+            this.Author = author;
+            this.Quantity = quantity;
+            this.Published = published;
+            this.Content = content;
+            this.Borrowed = 0;
+        }
+
+        public LibraryBook()
+            : this(new Isbn(), DateTime.Today, "Undefined")
+        {   }
+
+        public override string ToString()
+        {
+            return 
+                String.Format(
+                    " > ISBN: {0} \n" +
+                    " > Title: {1} \n" +
+                    " > Author: {2} \n" +
+                    " > Published: {3} \n" +
+                    " > Availble: " + (this.IsAvailible ? ("yes") : ("no")),
+                this.ISBN,
+                this.Title,
+                this.Author,
+                this.Published                
+                );
+        }
+
+        /// <summary>
+        /// Take one book from store
+        /// </summary>
+        /// <returns>success of operation</returns>
+        public bool Take()
+        {
+            if (this.IsAvailible)
+            {
+                this.Quantity--;
+                this.Borrowed++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Return()
+        {
+            if (this.IsAtHand)
+            {
+                this.Quantity++;
+                this.Borrowed--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Add to store count books
+        /// </summary>
+        /// <param name="count">number of books to be added (positive number)</param>
+        public void AddToStore(int count)
+        {
+            this.Quantity += (count > 0) ? count : 0;
+        }
+
+        public virtual void Read()
+        {
+            Console.WriteLine(this.Content);
+        }
+
+    }
+}
---
+++ programming/Lab9/Lab/Lab/ShopBook.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    class ShopBook : LibraryBook
+    {
+        public double Price
+        {
+            get;
+            private set;
+        }
+
+
+
+
+        public bool Buy()
+        {
+            if (this.IsAvailible)
+            {
+                this.Quantity--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+
+
+    //namespace lab9
+    //{
+    //    public class ShopTicket : Ticket
+    //    {
+    //        public int ItemsNumber { get; private set; }
+
+    //        private int[] Quantities { get; set; }
+
+    //        private double[] Prices { get; set; }
+
+    //        public ShopTicket()
+    //            : base()
+    //        {
+    //            ItemsNumber = 1;
+    //            Quantities = new int[ItemsNumber];
+    //            Prices = new double[ItemsNumber];
+    //        }
+
+    //        public ShopTicket(DateTime p_date, int p_itemsNumber, string p_shopName = "", string p_destination = "")
+    //            : base(p_shopName, p_date, 0, p_destination)
+    //        {
+    //            if (p_itemsNumber > 0)
+    //            {
+    //                ItemsNumber = p_itemsNumber;
+    //            }
+    //            else
+    //            {
+    //                ItemsNumber = 1;
+    //            }
+
+    //            Quantities = new int[ItemsNumber];
+    //            Prices = new double[ItemsNumber];
+
+    //            for (int i = 0; i < ItemsNumber; i++)
+    //            {
+    //                Console.Write("\n Enter {0} item quantity:  ", i + 1);
+    //                Quantities[i] = int.Parse(Console.ReadLine());
+    //                Console.Write("\n Enter {0} item price:  ", i + 1);
+    //                Prices[i] = double.Parse(Console.ReadLine());
+    //            }
+    //            CalculateTotal();
+    //        }
+
+    //        public ShopTicket(ShopTicket p_other)
+    //            : this(p_other.Date, p_other.ItemsNumber, p_other.Name, p_other.Destination)
+    //        {
+    //        }
+
+    //        private void CalculateTotal()
+    //        {
+    //            TotalBill = 0;
+    //            for (int i = 0; i < ItemsNumber; i++)
+    //            {
+    //                TotalBill += Quantities[i] * Prices[i];
+    //            }
+    //        }
+
+    //        public override string ToString()
+    //        {
+    //            string result = "";
+    //            if (Name != "")
+    //            {
+    //                result += string.Format("\n Welcome to \"{0}\"", Name);
+    //            }
+    //            if (Destination != "")
+    //            {
+    //                result += string.Format("\n {0}", Destination);
+    //            }
+    //            for (int i = 0; i < ItemsNumber; i++)
+    //            {
+    //                result += string.Format("\n {0}:\t{1} x {2:f2} $= {3:f2} $", i + 1, Quantities[i], Prices[i], Quantities[i] * Prices[i]);
+    //            }
+    //            result += string.Format("\n Total:  {0:f2} $\n", TotalBill);
+    //            return result;
+    //        }
+    //    }
+    //}
+
+    //static void Main(string[] args)
+    //{
+    //    DateTime d = new DateTime(1999, 7, 23);
+    //    Ticket t = new Ticket("Gas bill", d, 99.99, "Bad person");
+    //    Console.WriteLine(t);
+    //    while (!t.IsPaid)
+    //    {
+    //        t.Pay();
+    //        Console.WriteLine(t);
+    //    }
+
+    //    ShopTicket st = new ShopTicket(DateTime.Today, 3, "Goods");
+    //    Console.WriteLine(st);
+    //    while (!st.IsPaid)
+    //    {
+    //        st.Pay();
+    //        Console.WriteLine(st);
+    //    }
+    //    Console.ReadLine();
+    //}
+
+    //" > " + (this.IsAvailible? ("Price: {3}") : ("No prices")) + "\n" +
+
+
+
+    //    public virtual void Pay()
+    //    {
+    //        Console.Write("\n Enter your sum: ");
+    //        double payResult = double.Parse(Console.ReadLine());
+    //        payResult = this.PayResult(payResult);
+    //        if (payResult >= 0)
+    //        {
+    //            Console.WriteLine("\n Your change is {0:f2} $", payResult);
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("\n You didn't make it. You need {0:f2} $ more.", -payResult);
+    //        }
+    //    }
+    //}
+    //}
+
+
+
+
+}
